769f558f41c04907c4023fb9f3e35c761257989c
Lorenzo Scalzini <lorenzo@Mecche.lan>
2023-09-29 19:16:02 +0200

    Unique filter kmers added

--- table_counts.R
+++ table_counts.R
@@ -1,44 +1,55 @@
-# Carica la libreria tidyr per la manipolazione dei dati
+# Load the tidyr library for data manipulation
 library(tidyr)
 
-# Controlla se è stato fornito un argomento per la directory da linea di comando
+# Check if a command-line argument for the directory has been provided
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) == 0) {
-  stop("Devi specificare la directory come argomento da linea di comando.")
+  stop("You must specify the directory as a command-line argument.")
 }
 
-# Prendi la directory dall'argomento della linea di comando
+# Get the directory from the command-line argument
 directory <- args[1]
 
-# Ottieni la lista dei file nella directory con estensione .txt
+# Get a list of files in the directory with a .txt extension
 file_list <- list.files(path = directory, pattern = "\\.txt$", full.names = TRUE)
 
-# Inizializza un elenco vuoto per memorizzare i dati da tutti i file
+# Initialize an empty list to store data from all files
 data_list <- list()
 
-# Leggi e elabora i dati da ciascun file
-for (file_path in file_list) {
-  # Leggi il file come un dataframe
-  data <- read.table(file_path, header = FALSE, col.names = c("Kmer", "Counts"))
+# Read and process data from each file
+for (file in file_list) {
+  # Read the file into a dataframe
+  data <- read.table(file, header = FALSE, col.names = c("Kmer", "Counts"))
   
-  # Aggiungi il nome del file come colonna
-  data$File <- basename(file_path)
+  # Add the file name as a column
+  data$File <- basename(file)
   
-  # Aggiungi il dataframe alla lista
+  # Add the dataframe to the list
   data_list[[length(data_list) + 1]] <- data
 }
 
-# Unisci tutti i dati in un unico dataframe
+# Combine all the data into a single dataframe
 combined_data <- do.call(rbind, data_list)
 
-# Riempire i valori mancanti con 0
+# Fill missing values with 0
 combined_data_filled <- pivot_wider(combined_data, names_from = File, values_from = Counts, values_fill = 0)
 
-# Ordina le righe in base al Kmer
+# Sort rows based on Kmer
 combined_data_filled <- combined_data_filled[order(combined_data_filled$Kmer),]
 
-# Filtra escludendo i kmers che non compaiono in tutti i campioni
-kmers <- combined_data_filled[rowSums(combined_data_filled != 0) == ncol(combined_data_filled), ]
+# Filter by excluding kmers that do not appear in all samples
+#kmers <- combined_data_filled[rowSums(combined_data_filled != 0) == ncol(combined_data_filled), ]
 
-# Scrivi il dataframe 'kmers' in una tabella con separatore di tabulazione
-write.table(kmers, file = "output.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+# Copy the dataframe to unique_kmers
+unique_kmers <- combined_data_filled
+
+# Filter rows where the count is present in only one sample and 0 in all others (excluding the first column)
+unique_kmers <- unique_kmers[rowSums(unique_kmers[, -1] != 0) == 1, ]
+
+# Sort the dataframe based on counts (excluding the first column)
+unique_kmers <- unique_kmers[order(rowSums(unique_kmers[, -1]), decreasing = TRUE), ]
+
+
+# Write the 'kmers' dataframe to a tab-separated table
+write.table(unique_kmers, file = "output/unique_table.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
+
