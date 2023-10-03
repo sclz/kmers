afb0f517c437dd61f80a60076004a51314ab363c
sclz <62562592+sclz@users.noreply.github.com>
2023-10-03 17:26:13 +0200

    Update kmers_count.sh
    Bedtools bam to fastq conversion added

--- kmers_count.sh
+++ kmers_count.sh
@@ -7,7 +7,7 @@ fi
 
 #kmc_docker="docker run -v /Users/lorenzo/Desktop/kmers-count/script:/home -w /home --rm quay.io/biocontainers/kmc:3.0.0--2"
 kmc_docker="docker run -v $(pwd):/home -w /home --rm quay.io/biocontainers/kmc:3.0.0--2"
-
+bedtools_docker="docker run -v $(pwd):/home -w /home --rm quay.io/biocontainers/bedtools:2.31.0--h468198e_1"
 
 kmer="${1}"
 input_folder="${2}"
@@ -18,15 +18,19 @@ tmp="${4}"
 mkdir -p "${output_folder}"
 
 # Process all .fastq and .fastq.gz files in the input folder
-for file in "${input_folder}"/*.fastq "${input_folder}"/*.fastq.gz; do
-  sample="${file}"
+for file in "${input_folder}"/*.bam; do
+  bam="${file}"
   # Extract the filename without extension
-  filename=$(basename -- "$sample")
+  filename=$(basename -- "$bam")
   filename_without_extension="${filename%.*}"
+  # Build the fastq filename
+  fastq="${input_folder}/${filename_without_extension}.fq"
+  # Convert the BAM file to FASTQ format using bedtools
+  ${bedtools_docker} bedtools bamtofastq -i ${bam} -fq ${fastq}
   # Build the output filename with the "_kmers" extension
   out="${output_folder}/${filename_without_extension}_kmers"
   # Execute kmc inside the loop for each input file
-  ${kmc_docker} kmc -k${kmer} -ci1 -cs999999 "${sample}" "${out}" "${tmp}" 
+  ${kmc_docker} kmc -k${kmer} -ci1 -cs999999 "${fastq}" "${out}" "${tmp}" 
   # Execute kmc_tools inside the loop for each output file
   ${kmc_docker} kmc_tools transform "${out}" dump "${out}.txt"
 done
